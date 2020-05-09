1e0e5de951376b1d7452e98c346bdf7709bcaffd
Hassan Latif <ranashb520@gmail.com>
2020-05-09 16:01:05 +0500

    Few javascript functions have been added

---
+++ script.js
@@ -0,0 +1,12 @@
+function changecolor(){
+
+var bac=document.getElementById("bc").value;
+
+document.body.style.background= bac;
+
+       }
+
+function changeParaColor(){
+    var bac=document.getElementById("fc").value;   
+    document.getElementById("para").style.background= bac;
+}
