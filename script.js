ff590891c9a1dba62a9fd44d8ce12c72cd882a20
Hassan Latif <ranashb520@gmail.com>
2020-05-09 22:47:29 +0500

    Assignment has been completed

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function changeParaColor(){
     var bac=document.getElementById("fc").value;   
     document.getElementById("para").style.background= bac;
 }
+
+function changeFontFamily(){
+    var fontt = document.getElementById("fonts").value;
+    document.getElementById("para").style.fontFamily=fontt;  
+}
+
+function changeFont(){
+    var siz=document.getElementById("fz").value;
+    document.getElementById("para").style.fontSize=siz;
+}
+function changePadding(){
+    var pad=document.getElementById("pad").value;
+    document.getElementById("para").style.padding=pad;
+}
+function changeMargin(){
+    var mar=document.getElementById("marg").value;
+    document.getElementById("para").margin=mar;
+}
